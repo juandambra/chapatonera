a0c14218be032167fd9dce9294c91348ac26333b
juan dambra <juan.dambra@gmail.com>
2018-03-19 00:51:36 -0300

    init

---
+++ myScript.js
@@ -0,0 +1,79 @@
+function risa() {
+var audio = new Audio('Audios/Risa.mp3');
+audio.autobuffer = true;
+
+audio.play();
+}
+
+function andacaga() {
+var audio = new Audio('Audios/Andacaga.mp3');
+audio.autobuffer = true;
+audio.play();
+}
+
+function bomba() {
+var audio = new Audio('Audios/Bomba.mp3');
+
+audio.autobuffer = true;
+
+audio.play();
+}
+
+function fantino() {
+var audio = new Audio('Audios/Fantino.mp3');
+
+audio.autobuffer = true;
+
+audio.play();
+}
+
+function caraPiedra() {
+var audio = new Audio('Audios/Cara de piedra.mp3');
+audio.autobuffer = true;
+
+audio.play();
+}
+
+function andacagaCorto() {
+var audio = new Audio('Audios/AndacagaCorto.mp3');
+audio.autobuffer = true;
+
+audio.play();
+}
+function heredia() {
+var audio = new Audio('Audios/Heredia.mp3');
+audio.autobuffer = true;
+
+audio.play();
+}
+function humildad() {
+var audio = new Audio('Audios/Humildad.mp3');
+audio.autobuffer = true;
+
+audio.play();
+}
+function laPija() {
+var audio = new Audio('Audios/La pija.mp3');
+audio.autobuffer = true;
+
+audio.play();
+}
+function pasion() {
+var audio = new Audio('Audios/Pasion de Futbol.mp3');
+audio.autobuffer = true;
+
+audio.play();
+}
+function sinRespuesta() {
+var audio = new Audio('Audios/Sin Respuesta.mp3');
+audio.autobuffer = true;
+
+audio.play();
+}
+function sinRespuesta2() {
+var audio = new Audio('Audios/Sin Respuesta2.mp3');
+audio.autobuffer = true;
+
+audio.play();
+}
+
